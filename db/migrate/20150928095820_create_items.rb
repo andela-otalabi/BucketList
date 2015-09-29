d0a67b1b557fef28d7ee4635f5998fb36a8b1755
Andela Developer <andeladev@users-MacBook-Pro-3.local>
2015-09-29 16:06:58 +0100

    add items to a bucketlist

--- db/migrate/20150928095820_create_items.rb
+++ db/migrate/20150928095820_create_items.rb
@@ -3,7 +3,7 @@ class CreateItems < ActiveRecord::Migration
     create_table :items do |t|
       t.string :name
       t.boolean :done
-      belongs_to :bucketlist
+      
       t.timestamps null: false
     end
   end
