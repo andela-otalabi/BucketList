ed90ede7411b93217c72d6633f102750ebb9f41c
Andela Developer <andeladev@users-MacBook-Pro-3.local>
2015-10-02 14:19:44 +0100

    password confirmation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -43,7 +43,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
     def user_params
-      params.permit(:name, :email, :password)
+      params.permit(:name, :email, :password, :password_confirmation)
     end
 
 end
